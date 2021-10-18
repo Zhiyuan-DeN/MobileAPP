b597241ad9f8d5f46f35ce01b0453f90c7161f45
Rmnesia <30769692+Rmnesia@users.noreply.github.com>
2021-10-18 13:47:52 +0800

    Profile and Database drawback

--- app/src/main/java/com/example/mobileapplication/ProfileActivity.java
+++ app/src/main/java/com/example/mobileapplication/ProfileActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.mobileapplication.database.DatabaseModel;
+import com.example.mobileapplication.database.User;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -13,17 +16,27 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        String username = "";//这里写上你从前端得到的name
+        User user = DatabaseModel.getInstance().getUserInfo(username,new DatabaseModel.RequestResponse() {
+            @Override
+            public void onSuccess(User user) {
+                Toast.makeText(getApplicationContext(), "Register Successful", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Toast.makeText(getApplicationContext(), "Register Failed",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
         //jump to edit page
         Button profileEditButton = findViewById(R.id.edit_profile);
-        Intent intent = getIntent();
-        final String userName = intent.getStringExtra("userName");
         profileEditButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),ProfileEditActivity.class);
-                if (userName != null) {
-                    i.putExtra("userName", userName);
-                }
                 startActivity(i);
 
             }
--- app/src/main/java/com/example/mobileapplication/database/DatabaseModel.java
+++ app/src/main/java/com/example/mobileapplication/database/DatabaseModel.java
@@ -1,14 +1,20 @@
 package com.example.mobileapplication.database;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class DatabaseModel {
     private final String TAG = "DatabaseModel";
@@ -27,47 +33,33 @@ public class DatabaseModel {
         return instance;
     }
 
-    public void getUserProfile(String userName, String password, RequestResponse requestResponse) {
+    public void requestLogin(String userName, String password, RequestResponse requestResponse) {
         db.collection("Users")
-                .document(userName)
                 .get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        User user = documentSnapshot.toObject(User.class);
-                        if (user == null) {
-                            requestResponse.onError(new RuntimeException());
-                        } else if (user.getUserName().equals(userName) && user.getPassWord().equals(password)) {
-                            requestResponse.onSuccess(user);
-                        } else {
-                            requestResponse.onError(new RuntimeException());
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map<String, Object> data = document.getData();
+                                if (Objects.equals(data.get("UserName"), userName) && Objects.equals(data.get(
+                                        "Password"), password)) {
+                                    requestResponse.onSuccess(new User(
+                                            data.get("Description").toString(),
+                                            data.get("Email").toString(),
+                                            data.get("Password").toString(),
+                                            data.get("PhoneNum").toString(),
+                                            data.get("UserID").toString(),
+                                            data.get("UserName").toString(),
+                                            data.get("location").toString()
+                                    ));
+                                    return;
+                                }
+                            }
                         }
+                        requestResponse.onError(new RuntimeException("login error"));
                     }
                 })
-                /* .addOnSuccessListener(new OnCompleteListener<DocumentSnapshot>() {
-                     @Override
-                     public void onComplete(@NonNull DocumentSnapshot task) {
-                         if (task.isSuccessful()) {
-                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                 Map<String, Object> data = document.getData();
-                                 if (Objects.equals(data.get("UserName"), userName) && Objects.equals(data.get(
-                                         "Password"), password)) {
-                                     requestResponse.onSuccess(new User(
-                                             data.get("Description").toString(),
-                                             data.get("Email").toString(),
-                                             data.get("Password").toString(),
-                                             data.get("PhoneNum").toString(),
-                                             data.get("UserID").toString(),
-                                             data.get("UserName").toString(),
-                                             data.get("location").toString()
-                                     ));
-                                     return;
-                                 }
-                             }
-                         }
-                         requestResponse.onError(new RuntimeException("cant match any one."));
-                     }
-                 })*/
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
@@ -78,12 +70,11 @@ public class DatabaseModel {
 
     public void requestRegister(User user, RequestResponse requestResponse) {
         db.collection("Users")
-                .document(user.getUserName())
-                .set(user)
-                //.add(user.toMap())
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                .add(user.toMap())
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
-                    public void onSuccess(Void unused) {
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
                         requestResponse.onSuccess(user);
                     }
                 })
@@ -95,27 +86,37 @@ public class DatabaseModel {
                 });
     }
 
-    public void updateUserProfile(String oldUserName, String userName, String description,
-                                  String email,
-                                  String location, RequestResponse requestResponse) {
-        DocumentReference user = db.collection("Users")
-                .document(oldUserName);
-        HashMap<String, Object> map = new HashMap();
-        map.put("userName", userName);
-        map.put("description", description);
-        map.put("email", email);
-        map.put("location", location);
-        user.update(map).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                requestResponse.onSuccess(null);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                requestResponse.onError(e);
-            }
-        });
+    public User getUserInfo(String userName, RequestResponse requestResponse) {
+        User user = new User();
+        db.collection("Users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map<String, Object> data = document.getData();
+                                if (Objects.equals(data.get("UserName"), userName)) {
+                                    user.setDescription(data.get("Description").toString());
+                                    user.setEmail(data.get("Email").toString());
+                                    user.setPassWord(data.get("Password").toString());
+                                    user.setPhoneNum(data.get("PhoneNum").toString());
+                                    user.setUserId(data.get("UserID").toString());
+                                    user.setUserName(data.get("UserName").toString());
+                                    user.setLocation(data.get("location").toString());
+                                }
+                            }
+                        }
+                        requestResponse.onError(new RuntimeException("login error"));
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        requestResponse.onError(e);
+                    }
+                });
+        return user;
     }
 
     public interface RequestResponse {

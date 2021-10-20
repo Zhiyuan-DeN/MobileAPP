dc8ec95c1498c09eb9ab94f3bc106dd70b7d880e
Weixx433 <xiaohan.college@gmail.com>
2021-10-20 15:42:05 +0800

    add recorder UI

---
+++ app/src/main/java/com/example/mobileapplication/RecordActivity.java
@@ -0,0 +1,29 @@
+package com.example.mobileapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecordActivity extends AppCompatActivity {
+    ImageButton back_to_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording);
+
+        //back to main page
+        back_to_main = (ImageButton)findViewById(R.id.record_back_to_main);
+        back_to_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RecordActivity.this.finish();
+            }
+        });
+
+    }
+}

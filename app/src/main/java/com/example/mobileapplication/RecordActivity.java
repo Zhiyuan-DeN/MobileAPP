d20ec2986c61ffb9b5c5240ebb77393d586b4b7c
JIAFUZ0607 <70121177+JIAFUZ0607@users.noreply.github.com>
2021-10-21 15:01:56 +0800

    Update RecordActivity.java

--- app/src/main/java/com/example/mobileapplication/RecordActivity.java
+++ app/src/main/java/com/example/mobileapplication/RecordActivity.java
@@ -8,13 +8,57 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mobileapplication.record.Recorder;
+
+import java.io.File;
+import java.io.IOException;
+
 public class RecordActivity extends AppCompatActivity {
     ImageButton back_to_main;
+    ImageButton start;
+    ImageButton pause;
 
+    File file;
+    Recorder r = new Recorder();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recording);
+
+        //start button
+        start = (ImageButton)findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        r.setRecording(true);
+                        file = r.startRecord();
+
+                    }
+                });
+                RecordActivity.this.finish();
+            }
+        });
+
+
+
+        //pause button
+        pause = (ImageButton)findViewById(R.id.pause);
+        pause.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(file.exists()){
+                    try {
+                        r.playRecord(file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                RecordActivity.this.finish();
+            }
+        });
 
         //back to main page
         back_to_main = (ImageButton)findViewById(R.id.record_back_to_main);
@@ -25,5 +69,11 @@ public class RecordActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        r.setRecording(false);
+        r.releaseAudioTrack();
     }
 }

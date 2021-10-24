e2e04a08db31135de7a87edc10da978dc80d48fc
zoel728 <88759746+zoel728@users.noreply.github.com>
2021-10-24 18:14:16 +1100

    working on fragments

--- app/src/main/java/com/example/mobileapplication/RecordFragment.java
+++ app/src/main/java/com/example/mobileapplication/RecordFragment.java
@@ -1,4 +1,46 @@
 package com.example.mobileapplication;
 
-public class RecordFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mobileapplication.R;
+import com.example.mobileapplication.databinding.FragmentRecordBinding;
+
+public class RecordFragment extends Fragment {
+
+    private RecordViewModel recordViewModel;
+    private FragmentRecordBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        recordViewModel =
+                new ViewModelProvider(this).get(RecordViewModel.class);
+
+        binding = FragmentRecordBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textRecord;
+        recordViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
---
+++ app/src/main/java/com/example/mobileapplication/RecordViewModel.java
@@ -0,0 +1,19 @@
+package com.example.mobileapplication;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RecordViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public RecordViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
